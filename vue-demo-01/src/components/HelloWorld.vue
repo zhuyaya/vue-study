dde92fc957785072d9aec1c72299edc920671e1f
zhuyaya <15601937556@163.com>
2024-04-23 22:49:55 +0800

    万物起源，HelloWorld

--- vue-demo-01/src/components/HelloWorld.vue
+++ vue-demo-01/src/components/HelloWorld.vue
@@ -1,44 +1,18 @@
+<!-- 所有的开始 Hello World -->
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
+import {ref} from "vue";
+
+/**
+ * “ref”是用来存储值的响应式数据源。
+ * 理论上我们在展示该字符串的时候不需要将其包装在 ref() 中，更改这个值的时候，我们就需要它了。
+ */
+const message = ref("Hello World!")
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+  <h1>{{ message }}</h1>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
